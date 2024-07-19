bca245fc74695e5c7a71afa34c934ea92182975a
Dustin Deyoung <ddeyoung@morpheusdata.com>
2024-07-19 16:41:37 -0400

    Fix plugin register.

--- src/main/groovy/com/morpheusdata/upcloud/UpcloudProvisionProvider.groovy
+++ src/main/groovy/com/morpheusdata/upcloud/UpcloudProvisionProvider.groovy
@@ -297,7 +297,7 @@ class UpcloudProvisionProvider extends AbstractProvisionProvider implements Work
 	 */
 	@Override
 	String getCode() {
-		return 'upcloud'
+		return PROVISION_PROVIDER_CODE
 	}
 
 	/**
